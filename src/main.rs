a6ca06498ca1863ecb2968d847c4170fae21c3ff
Layl <LaylConway@users.noreply.github.com>
2018-01-05 19:16:19 +0100

    :sunrise: Mk II

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,63 @@
 extern crate ggez;
 
-use ggez::{event, graphics, Conf, Context, MainState};
-use ggez::graphics::{DrawMode, Point2};
+use ggez::{Context, GameResult};
+use ggez::conf::{Conf, WindowMode, WindowSetup};
+use ggez::event;
+use ggez::graphics;
+use std::env;
+use std::path;
 
 struct MainState {
-    pos_x: f32,
 }
 
 impl MainState {
-    fn new(_ctx: &mut Context) -> GameResult {
-        let s = MainState { pos_x: 0.0 };
+    fn new(_ctx: &mut Context) -> GameResult<MainState> {
+        let s = MainState {};
         Ok(s)
     }
 }
 
+
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx);
-        graphics::circle(ctx,
-                         DrawMode::Fill,
-                         Point2::new(self.pos_x, 380.0),
-                         100.0,
-                         2.0)?;
+
+        // Draw here
+
         graphics::present(ctx);
         Ok(())
     }
 }
 
 pub fn main() {
-    let c = conf::Conf::new();
-    let ctx = &mut Context::load_from_conf("super_simple", "ggez", c).unwrap();
+    let mut c = Conf::new();
+    c.window_mode = WindowMode {
+        width: 1280,
+        height: 720,
+        .. Default::default()
+    };
+    c.window_setup = WindowSetup {
+        title: "Space Game".into(),
+        .. Default::default()
+    };
+    let ctx = &mut Context::load_from_conf("space-game", "carbidegames", c).unwrap();
+
+    // We add the CARGO_MANIFEST_DIR/resources do the filesystems paths so
+    // we we look in the cargo project for files.
+    if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        ctx.filesystem.mount(&path, true);
+    }
+
     let state = &mut MainState::new(ctx).unwrap();
-    event::run(ctx, state).unwrap();
+    if let Err(e) = event::run(ctx, state) {
+        println!("Error encountered: {}", e);
+    } else {
+        println!("Game exited cleanly.");
+    }
 }
